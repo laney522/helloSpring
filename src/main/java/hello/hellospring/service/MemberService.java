5ac4a5f76832135b82d12b59cfc825de95c4d736
laney522 <pm72222@gmail.com>
2022-04-13 16:44:47 +0900

    test Junit

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,8 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+}
